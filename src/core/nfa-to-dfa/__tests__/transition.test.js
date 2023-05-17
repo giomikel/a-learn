a376962ade0c7f6b95d8885f613bceec1206413c
giomikel <gmike19@freeuni.edu.ge>
2023-05-17 15:17:56 +0400

    Add unit test for Transition initialization

---
+++ src/core/nfa-to-dfa/__tests__/transition.test.js
@@ -0,0 +1,15 @@
+const Transition = require('../transition');
+
+function testTransition() {
+    const transition = new Transition(0, '0', 1);
+    if (transition.fromState !== 0 ||
+        transition.symbol !== '0' ||
+        transition.toState !== 1
+    ) {
+        console.error('Transition initialization failed');
+    } else {
+        console.log('Transition initialization passed');
+    }
+}
+
+testTransition();
