a033d0ca9c8256eeaa437a6190cb132cc18b5c56
giomikel <gmike19@freeuni.edu.ge>
2023-05-17 14:37:20 +0400

    Add nfa class

---
+++ src/core/nfa-to-dfa/nfa.js
@@ -0,0 +1,21 @@
+class NFA {
+    constructor(states, transitions, acceptStates) {
+        this.states = states;   // Array of states, e.g. [0, 1, 2, ...] Where initial state is the lowest value
+        this.transitions = transitions;
+        this.acceptStates = acceptStates;
+        this.alphabet = getAlphabetFromTransitions(transitions);
+        this.startState = getStartStateFromStates(states);
+    }
+}
+
+function getAlphabetFromTransitions(transitions) {
+    const alphabet = new Set();
+    transitions.forEach(element => {
+        alphabet.add(element.symbol);
+    });
+    return Array.from(alphabet);
+}
+
+function getStartStateFromStates(states) {
+    return Math.min(...states);
+}
