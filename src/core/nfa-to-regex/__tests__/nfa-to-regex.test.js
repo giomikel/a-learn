3daa8c41e2db27734ae68c7a0f492dfcf9b8f29b
giomikel <gmike19@freeuni.edu.ge>
2023-08-05 22:00:30 +0400

    Add test cases

--- src/core/nfa-to-regex/__tests__/nfa-to-regex.test.js
+++ src/core/nfa-to-regex/__tests__/nfa-to-regex.test.js
@@ -17,5 +17,43 @@ test('test nfa to regex conversion', () => {
         acceptStates
     );
     const result = convertNFAToRegex(nfa);
-    console.log(result);
+    expect(result).toEqual('(0|1)*');
+})
+
+test('test nfa to regex conversion', () => {
+    const states = [0, 1, 2, 3];
+    const transitions = [
+        new Transition(0, EPSILON_SYMBOL, 1),
+        new Transition(1, 'a', 1),
+        new Transition(1, 'b', 2),
+        new Transition(2, 'a', 2),
+        new Transition(2, 'b', 2),
+        new Transition(2, EPSILON_SYMBOL, 3)
+    ];
+    const acceptStates = [3];
+    const nfa = new FiniteStateMachine(
+        states,
+        transitions,
+        acceptStates
+    );
+    const result = convertNFAToRegex(nfa);
+    expect(result).toEqual('(a*b)(a|b)*');
+})
+
+test('test nfa to regex conversion', () => {
+    const states = [0, 1, 2];
+    const transitions = [
+        new Transition(0, 'a', 0),
+        new Transition(0, 'b', 0),
+        new Transition(0, 'a', 1),
+        new Transition(1, 'b', 2)
+    ];
+    const acceptStates = [2];
+    const nfa = new FiniteStateMachine(
+        states,
+        transitions,
+        acceptStates
+    );
+    const result = convertNFAToRegex(nfa);
+    expect(result).toEqual('((a|b)*a)b');
 })
