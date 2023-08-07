e819a4c07a91607f15b4c922ba5eb4513efa9684
giorgi <giorgi.abuladze@azry.com>
2023-08-08 02:33:28 +0400

    create cfg structure

---
+++ src/core/structures/cfg.mjs
@@ -0,0 +1,33 @@
+class CFG {
+    constructor() {
+      this.terminals = new Set();
+      this.nonTerminals = new Set();
+      this.productionRules = new Map();
+      this.startSymbol = null;
+    }
+  
+    addTerminal(terminal) {
+      this.terminals.add(terminal);
+    }
+  
+    addNonTerminal(nonTerminal) {
+      this.nonTerminals.add(nonTerminal);
+    }
+  
+    addProductionRule(nonTerminal, production) {
+      if (!this.productionRules.has(nonTerminal)) {
+        this.productionRules.set(nonTerminal, []);
+      }
+      this.productionRules.get(nonTerminal).push(production);
+    }
+  
+    setStartSymbol(startSymbol) {
+      if (!this.nonTerminals.has(startSymbol)) {
+        throw new Error("Start symbol must be a non-terminal in the grammar.");
+      }
+      this.startSymbol = startSymbol;
+    }
+  }
+
+  export default CFG;
+  
