fdb70f016a54d252287b3cb223606b31732198d3
giorgi <giorgi.abuladze@azry.com>
2023-08-12 00:40:33 +0400

    implement dfa-simulator class

---
+++ src/core/dfa-simulation/dfa-simulation.mjs
@@ -0,0 +1,57 @@
+
+class DFASimulator {
+    constructor(dfa) {
+        this.dfa = dfa; 
+        this.currentState = dfa.startState;
+        this.currentInputIndex = 0; 
+        this.input = '';
+    }
+
+    setInput(input) {
+        this.currentInputIndex = 0;
+        this.input = input;
+    }
+
+    step() {
+        if (this.currentInputIndex >= this.input.length) {
+            return false;
+        }
+        const transition = this.getTransition();
+        if (transition !== null){
+            this.currentState = transition.toState;
+            this.currentInputIndex += 1;
+            return true;
+        }
+        return false;  
+    }
+
+    getTransition(){
+        for(let transition of this.dfa){
+            if (transition.fromState === this.currentState && transition.symbol === this.input[this.currentInputIndex]){
+                return transition;
+            }
+        }
+        return  null;
+    }
+
+    isAcceptState() {
+        return this.nfa.acceptStates.includes(this.currentState);
+    }
+
+    getCurrentInputSymbol() {
+        if (this.currentInputIndex >= this.input.length) {
+            return null;
+        }
+        return this.input[this.currentInputIndex];
+    }
+
+    accepts(input) {
+        this.setInput(input);
+
+        while (this.step());
+
+        return this.isAcceptState();
+    }
+}
+
+export { DFASimulator }
