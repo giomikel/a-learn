f2909d730393865d33db83817a9568412ba79fc0
giorgi <giorgi.abuladze@azry.com>
2023-08-15 09:24:05 +0400

    create NFAToRegex and RegexToNFA components

---
+++ src/component/RegexToNFA.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "../css/RegexToNFA.css";
+
+function RegexToNFA() {
+  return (
+    <div className="regex-to-nfa-container">
+      <h2 className="section-title">Regex to NFA</h2>
+      <p className="section-description">
+        Convert Regular Expression to Non-Deterministic Finite Automata (NFA).
+      </p>
+    </div>
+  )
+}
+
+export default RegexToNFA;
