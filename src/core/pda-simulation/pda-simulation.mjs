cd28a6a7d54ad9dfbc9d5dd6f753f7e77221c548
giomikel <gmike19@freeuni.edu.ge>
2023-08-16 16:45:48 +0400

    Fix stack bug

--- src/core/pda-simulation/pda-simulation.mjs
+++ src/core/pda-simulation/pda-simulation.mjs
@@ -4,14 +4,14 @@ class PDASimulator {
     constructor(pda) {
         this.pda = pda;
         this.currentStacks = new Map();
-        this.currentStates = this.findPDAEpsilonClosures([this.pda.startState], this.pda.transitions);
+        this.currentStates = this.findPDAEpsilonClosures([this.pda.startState], this.pda.transitions, this.currentStacks);
         this.currentInputIndex = 0;
         this.input = '';
     }
 
     reset() {
         this.currentStacks = new Map();
-        this.currentStates = this.findPDAEpsilonClosures([this.pda.startState], this.pda.transitions);
+        this.currentStates = this.findPDAEpsilonClosures([this.pda.startState], this.pda.transitions, this.currentStacks);
         this.currentInputIndex = 0;
         this.input = '';
     }
@@ -21,7 +21,7 @@ class PDASimulator {
         this.input = input;
     }
 
-    findPDAEpsilonClosures(states, transitions) {
+    findPDAEpsilonClosures(states, transitions, nextStacks) {
         const closures = new Set(states);
 
         function dfsEpsilon(state, currentStacks, arraysInclude) {
@@ -43,7 +43,7 @@ class PDASimulator {
                 }
             });
         }
-        states.forEach(state => dfsEpsilon(state, this.currentStacks, this.arraysInclude));
+        states.forEach(state => dfsEpsilon(state, nextStacks, this.arraysInclude));
         return Array.from(closures);
     }
 
@@ -51,17 +51,6 @@ class PDASimulator {
         if (this.currentInputIndex < this.input.length) {
             const symbol = this.input[this.currentInputIndex];
             const nextStates = [];
-
-            this.currentStates.forEach(st => {
-                const transitions = this.pda.transitions.filter(transition => transition.fromState == st && transition.inputSymbol == symbol);
-                transitions.forEach(t => {
-                    this.currentStacks.forEach(s => {
-                        if (s[s.length - 1] == t.popSymbol || t.popSymbol == EPSILON_SYMBOL) {
-                            this.currentStacks.delete(t.toState);
-                        }
-                    });
-                });
-            });
 
             const nextStacks = new Map();
 
@@ -73,20 +62,25 @@ class PDASimulator {
                     currentStateStacks.forEach(s => {
                         if (s[s.length - 1] == t.popSymbol || t.popSymbol == EPSILON_SYMBOL) {
                             toStates.add(t.toState);
-                            const toStateStacks = [...(this.currentStacks.get(t.toState) || [])];
+                            const toStateStacks = [...(nextStacks.get(t.toState) || [])];
                             const sClone = [...s];
                             if (t.popSymbol != EPSILON_SYMBOL) { sClone.pop(); }
                             if (t.pushSymbol != EPSILON_SYMBOL) { sClone.push(t.pushSymbol); }
                             if (!this.arraysInclude(toStateStacks, sClone)) { toStateStacks.push(sClone); }
-                            this.nextStacks.set(t.toState, toStateStacks);
+                            nextStacks.set(t.toState, toStateStacks);
                         }
                     });
                 });
-                nextStates.push(...this.findPDAEpsilonClosures(transitions.map(t => t.toState), this.pda.transitions));
+                nextStates.push(...this.findPDAEpsilonClosures(transitions.map(t => t.toState), this.pda.transitions, nextStacks
+                ));
             }
             this.currentStacks.forEach((value, key) => {
-
-            })
+                if (nextStacks.has(key)) {
+                    const nextStacksForState = nextStacks.get(key) || [];
+                    // const filteredStacks = nextStacksForState.filter(stack => value.some(arr => JSON.stringify(arr) === JSON.stringify(second)));
+                    this.currentStacks.set(key, nextStacksForState);
+                }
+            });
             this.currentStates = Array.from(new Set(nextStates)).sort();
             this.currentInputIndex++;
             return this.currentStates.length > 0;
