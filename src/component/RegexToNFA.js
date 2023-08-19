039b20ec02b8673142addd96e1895ecde3b4ca10
giorgi <giorgi.abuladze@azry.com>
2023-08-19 13:08:35 +0400

    validate regex expression

--- src/component/RegexToNFA.js
+++ src/component/RegexToNFA.js
@@ -3,7 +3,7 @@ import "../css/RegexToNFA.css";
 import { useEffect, useRef } from 'react';
 import * as d3 from 'd3';
 import regexToNFA from '../core/regex-to-nfa/regex-to-nfa.mjs';
-
+import validateExpression from '../core/regex-to-nfa/regex-validator.mjs';
 
 function FSMVisualization({ fsm }) {
   const svgRef = useRef(null); 
@@ -53,8 +53,6 @@ function FSMVisualization({ fsm }) {
       link.source = nodes[link.source] || (nodes[link.source] = { name: 'q'+link.source });
       link.target = nodes[link.target] || (nodes[link.target] = { name: 'q'+link.target });
     });
-
-    console.log(nodes);
 
     const w = 600,
       h = 600;
@@ -201,36 +199,44 @@ function generateNFAFromRegex(regex) {
 function RegexToNFA() {
   const [regex, setRegex] = useState('');
   const [nfa, setNFA] = useState(null);
+  const [validationError, setValidationError] = useState('');
 
   const handleGenerateNFA = () => {
-    const generatedNFA = generateNFAFromRegex(regex);
-    setNFA(generatedNFA); 
+    if (!validateExpression(regex)) {
+      setValidationError('Invalid regex. Please enter a valid regular expression.');
+      setNFA(null);
+    } else {
+      setValidationError('');
+      const generatedNFA = generateNFAFromRegex(regex);
+      setNFA(generatedNFA);
+    }
   };
 
   useEffect(() => {
     setNFA(null);
+    setValidationError('');
   }, [regex]);
-
   
   return (
     <div className="regex-to-nfa-container">
-    <h2 className="section-title">Regex to NFA</h2>
-    <p className="section-description">
-      Convert Regular Expression to Non-Deterministic Finite Automata (NFA).
-    </p>
-    <div className="input-container">
-      <input
-        type="text"
-        value={regex}
-        onChange={(e) => setRegex(e.target.value)}
-        placeholder="Enter regex..."
-      />
-      <button onClick={handleGenerateNFA}>Generate NFA</button>
+      <h2 className="section-title">Regex to NFA</h2>
+      <p className="section-description">
+        Convert Regular Expression to Non-Deterministic Finite Automata (NFA).
+      </p>
+      <div className="input-container">
+        <input
+          type="text"
+          value={regex}
+          onChange={(e) => setRegex(e.target.value)}
+          placeholder="Enter regex..."
+        />
+        <button onClick={handleGenerateNFA}>Generate NFA</button>
+      </div>
+      {validationError && <p className="error-message">{validationError}</p>}
+      <div className="nfa-visualization-container">
+        {nfa && <FSMVisualization fsm={nfa} />}
+      </div>
     </div>
-    <div className="nfa-visualization-container">
-      {nfa && <FSMVisualization fsm={nfa} />}
-    </div>
-  </div>
   )
 }
 
