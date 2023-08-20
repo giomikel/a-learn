a96947b43654f60824deb4149f704d97ca1f62f9
giorgi <giorgi.abuladze@azry.com>
2023-08-20 11:52:30 +0400

    add CFGToCNF route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import RegexToNFA from './component/regex-to-nfa/RegexToNFA';
 import NFAToRegex from './component/NFAToRegex';
 import NFAToDFA from './component/nfa-to-dfa/NFAToDFA';
 import RegexToDFA from './component/regex-to-dfa/RegexToDFA';
+import CFGToCNF from './component/cfg-to-cnf/CFGToCNF';
 import Home from './component/Home'
 import { Routes, Route } from 'react-router-dom'
 
@@ -16,6 +17,7 @@ function App() {
         <Route path="/NFAToRegex" element={<NFAToRegex />} />
         <Route path="/NFAToDFA" element={<NFAToDFA />} />
         <Route path="/RegexToDFA" element={<RegexToDFA />} />
+        <Route path="/CFGToCNF" element={<CFGToCNF />} />
       </Routes>
     </div>
   );
