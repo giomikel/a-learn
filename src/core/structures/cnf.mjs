677f2d2fc5323c7c6509b6e6f90cf6e43b594ec8
giorgi <giorgi.abuladze@azry.com>
2023-08-20 21:19:39 +0400

    add toString method

--- src/core/structures/cnf.mjs
+++ src/core/structures/cnf.mjs
@@ -3,6 +3,16 @@ class CNF {
         this.productionRules = new Map(productionRules);
         this.startSymbol = startSymbol;
     }
+
+    printRule(key, value) {
+        return `${key} -> ${value.join(' | ')}`;
+    }
+
+    toString() {
+        return `StartSymbol: ${this.startSymbol}
+Productions: 
+${Array.from(this.productionRules).map(([key, value]) => this.printRule(key, value)).join('\n')}`;
+    }
 }
 
 export default CNF
