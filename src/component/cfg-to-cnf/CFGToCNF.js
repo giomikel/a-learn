342fdcd12e095b3ba315ac0cba36594ffcbd6e5a
giorgi <giorgi.abuladze@azry.com>
2023-08-20 11:49:49 +0400

    create cfg to cnf conversion js and css files

---
+++ src/component/cfg-to-cnf/CFGToCNF.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "../css/CFGToCNF.css";
+
+function CFGToCNF() {
+  return (
+    <div className="cfg-to-cnf-container">
+      <h2 className="section-title">CFG to CNF</h2>
+      <p className="section-description">
+        Convert CFG to CNF.
+      </p>
+    </div>
+  )
+}
+
+export default CFGToCNF;
