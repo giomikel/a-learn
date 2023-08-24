68e0089fc4cbc68d9c2b86a6eafac17e15a340a0
giomikel <gmike19@freeuni.edu.ge>
2023-08-25 00:58:42 +0400

    Update class name

--- src/component/nfa-to-dfa/NFAToDFA.js
+++ src/component/nfa-to-dfa/NFAToDFA.js
@@ -67,7 +67,7 @@ function NFAToDFA() {
           <button onClick={handleCreateFSM}>Create FSM</button>
         </div>
         <div className="graph-visualization-scroll-container" id='graph-visualization-scroll-container' style={{ maxHeight: '90vh' }}>
-          <div className="fsm-visualization-container">
+          <div className="graph-visualization-container">
             {graph && <GraphVisualization graph={graph} />}
           </div>
         </div>
